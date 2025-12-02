9eefd732d8cd2882602768ef910f0c0ddd46d12b
Jacob de la Vega <161660037+GoodGuyRusty@users.noreply.github.com>
2025-12-01 18:38:54 -0600

    Update Left Rotation.cpp

--- Left Rotation.cpp
+++ Left Rotation.cpp
@@ -1,49 +1,116 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 // HackerRank (Easy) | Left Rotation | https://www.hackerrank.com/challenges/array-left-rotation/problem
 
-vector<int> rotLeft(int d, const vector<int>& arr) {
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+vector<int> rotateLeft(int d, vector<int> arr) {
     int n = arr.size();
-    if (n == 0) {
-        return {};
+
+    int rotations = d % n;
+    
+    vector<int> result(n);
+    
+    for (int i = 0; i < n; i++) {
+        int new_i = (i - rotations + n) % n;
+        
+        result[new_i] = arr[i];
     }
 
-    int k = d % n;
-
-    vector<int> rotated_arr(n);
-
-    for (int i = 0; i < n; ++i) {
-        
-        int new_index = (i - k + n) % n;
-        
-        rotated_arr[new_index] = arr[i];
-    }
-
-    return rotated_arr;
+    return result;
 }
 
-int main() {
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-    int n;
-    int d;
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
 
-    if (!(cin >> n >> d)) return 1;
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int d = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
 
     vector<int> arr(n);
-    for (int i = 0; i < n; ++i) {
-        if (!(cin >> arr[i])) return 1;
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
     }
 
-    vector<int> result = rotLeft(d, arr);
+    vector<int> result = rotateLeft(d, arr);
 
-    for (int i = 0; i < result.size(); ++i) {
-        cout << result[i] << (i == result.size() - 1 ? "" : " ");
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
     }
-    cout << "\n";
+
+    fout << "\n";
+
+    fout.close();
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
